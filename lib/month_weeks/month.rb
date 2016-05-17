45977a958a1377efb7d74ac2a83e2e3190799f70
Eric Watson <wasnotrice@gmail.com>
2016-05-17 01:09:50 -0500

    Add a few more methods

--- lib/month_weeks/month.rb
+++ lib/month_weeks/month.rb
@@ -2,12 +2,12 @@ require 'month_weeks/week'
 
 module MonthWeeks
   class Month
-    private
-    attr_reader :init_date
+    attr_reader :first_day_of_week
 
     public
-    def initialize(date)
+    def initialize(date, options = {})
       @init_date = Date.parse(date)
+      @first_day_of_week = options[:first_day_of_week] || 1
     end
 
     def year
@@ -19,8 +19,51 @@ module MonthWeeks
     end
 
     def weeks
-      week = Week.new(Date.new(year, month, 1), Date.new(year, month, 1))
+      endOfFirst = day_to_date(end_of_week(first))
+      puts endOfFirst.inspect
+      week = Week.new(first, endOfFirst)
       [week, 1, 2, 3, 4]
+    end
+
+    def first
+      day_to_date(1)
+    end
+
+    def last
+      day_to_date(days)
+    end
+
+    def last_day_of_week
+      (first_day_of_week + 6) % 7
+    end
+
+    def days
+      case month
+      when 1, 3, 5, 7, 8, 10, 12
+        31
+      when 4, 6, 9, 11
+        30
+      when 2
+        Date.leap? (year) ? 29 : 28
+      end
+    end
+    private
+    attr_reader :init_date
+
+    def end_of_week(start_of_week)
+      start_day = start_of_week.mday
+      if start_day == first.mday && last_day_of_week == first.mday
+        return start_of_week
+      end
+
+      puts last.inspect
+      puts start_day.inspect
+      [last, start_of_week + 6].min
+    end
+
+    def day_to_date(dayOrDate)
+      day = dayOrDate.respond_to?(:mday) ? dayOrDate.mday : dayOrDate
+      Date.new(year, month, day)
     end
   end
 end
--- spec/month_weeks_spec.rb
+++ spec/month_weeks_spec.rb
@@ -1,22 +1,38 @@
 require 'spec_helper'
 
 describe MonthWeeks do
-  it 'has a version number' do
+  specify 'has a version number' do
     expect(MonthWeeks::VERSION).not_to be nil
   end
 end
 
 describe 'May 2016' do
   let(:month) { MonthWeeks::Month.new('2016-05-15') }
-  it 'has year 2016' do
+  specify 'year is 2016' do
     expect(month.year).to eq(2016)
   end
 
-  it 'has month 5' do
+  specify 'month is 5' do
     expect(month.month).to eq(5)
   end
 
-  it 'has 5 weeks' do
+  specify 'first is 2015-05-01' do
+    expect(month.first).to eq(Date.new(2016, 5, 1))
+  end
+
+  specify 'last is 2015-05-31' do
+    expect(month.last).to eq(Date.new(2016, 5, 31))
+  end
+
+  specify 'first_day_of_week is 1' do
+    expect(month.first_day_of_week).to eq(1)
+  end
+
+  specify 'last_day_of_week is 0' do
+    expect(month.last_day_of_week).to eq(0)
+  end
+
+  specify 'has 5 weeks' do
     expect(month.weeks.length).to eq(5)
   end
 
@@ -24,6 +40,10 @@ describe 'May 2016' do
     let (:week) { month.weeks.first }
     it 'starts on 2016-05-01' do
       expect(week.start_date).to eq(Date.new(2016, 5, 1))
+    end
+
+    it 'ends on 2016-05-01' do
+      expect(week.end_date).to eq(Date.new(2016, 5, 1))
     end
   end
 end
